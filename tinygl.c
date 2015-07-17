a36ea2d6e2459442c2f219e53e3f6c02ff83f306
Adrian Sampson <adrian@radbox.org>
2015-07-16 18:09:37 -0700

    Consistent 2-space indentation

--- tinygl.c
+++ tinygl.c
@@ -10,17 +10,17 @@ GLuint create_shader() {
   // The vertex shader.
   GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
   const char *vertex_shader = " \
-      attribute vec4 position; \
-      attribute vec3 normal; \
-      attribute vec4 color; \
-      varying vec4 vColor; \
-      uniform float uniID; \
-      varying vec3 varyingnormal; \
-      void main() { \
-          vColor = position; \
-          gl_Position = position; \
-          varyingnormal = vec3(position.x,position.y,0.); \
-      } \
+    attribute vec4 position; \
+    attribute vec3 normal; \
+    attribute vec4 color; \
+    varying vec4 vColor; \
+    uniform float uniID; \
+    varying vec3 varyingnormal; \
+    void main() { \
+      vColor = position; \
+      gl_Position = position; \
+      varyingnormal = vec3(position.x,position.y,0.); \
+    } \
   ";
   glShaderSource(vshader, 1, &vertex_shader, 0);
   glCompileShader(vshader);
@@ -28,13 +28,13 @@ GLuint create_shader() {
   // The fragment (pixel) shader.
   GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
   const char *fragment_shader = " \
-      uniform float uniID; \
-      varying vec4 vColor; \
-      varying vec3 varyingnormal; \
-      void main() { \
-          float r2 = (vColor.x+1.)*(vColor.x+1.)+(vColor.y+1.)*(vColor.y+1.); \
-          gl_FragColor = vec4((vColor.x+1.)/r2,(vColor.y+1.)/r2,uniID,1.); \
-      } \
+    uniform float uniID; \
+    varying vec4 vColor; \
+    varying vec3 varyingnormal; \
+    void main() { \
+      float r2 = (vColor.x+1.)*(vColor.x+1.)+(vColor.y+1.)*(vColor.y+1.); \
+      gl_FragColor = vec4((vColor.x+1.)/r2,(vColor.y+1.)/r2,uniID,1.); \
+    } \
   ";
   glShaderSource(fshader, 1, &fragment_shader, 0);
   glCompileShader(fshader);
